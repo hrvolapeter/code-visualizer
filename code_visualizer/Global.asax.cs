55c7329120ad0c9b518bc801f034d358d003935a
Peter Hrvola <peter.hrvola@hotmail.com>
2017-04-25 21:22:04 +0200

    init

---
+++ code_visualizer/Global.asax.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Http;
+
+namespace code_visualizer
+{
+	public class Global : HttpApplication
+	{
+		protected void Application_Start()
+		{
+			GlobalConfiguration.Configure(WebApiConfig.Register);
+		}
+	}
+}
