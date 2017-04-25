16bacf7217fd80de69da3b055b88e51c6cca98c6
Peter Hrvola <peter.hrvola@hotmail.com>
2017-04-25 22:13:20 +0200

    added example endpoint for init

---
+++ code_visualizer/Controllers/InitController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+
+namespace code_visualizer.Controllers
+{
+	public class InitController : ApiController
+	{
+		public IHttpActionResult GetInit(int id)
+		{
+			return Ok("hello world"+id.ToString());
+		}
+	}
+}
