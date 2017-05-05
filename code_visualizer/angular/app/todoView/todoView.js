63b8e67bb45c8c15f6c40fc96f6dd91c1736dd52
Pavel Jezek <paveljezek@outlook.com>
2017-05-05 20:30:22 +0200

    added todoView

--- code_visualizer/angular/app/todoView/todoView.js
+++ code_visualizer/angular/app/todoView/todoView.js
@@ -1,42 +1,51 @@
 'use strict';
 
-angular.module('myApp.view1', ['ngRoute', 'xml'])
+angular.module('myApp.todoView', ['ngRoute', 'xml'])
 
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
-    templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
+  $routeProvider.when('/todoView', {
+    templateUrl: 'todoView/todoView.html',
+    controller: 'todoViewCtrl'
   });
 }])
 
-.controller('View1Ctrl', ['x2js', '$scope', '$http', function(x2js, $scope, $http) {
+.controller('todoViewCtrl', ['x2js', '$scope', '$http', function(x2js, $scope, $http) {
     $scope.input = "";
 
     $scope.initRepositories = function() {
     /* TODO: validate urls */
-    var urls = $scope.input.split('\n');
+    var url = $scope.input;
     var jsonObj = {
         'Repositories' : {
             '_xmlns': 'http://schemas.datacontract.org/2004/07/',
             '_xmlns:s': 'http://schemas.microsoft.com/2003/10/Serialization/Arrays',
             '_xmlns:i': 'http://www.w3.org/2001/XMLSchema-instance',
             'Repository' : {
-              's:string': urls
+              's:string': url
             }
         }
     };
+    console.log(jsonObj);
     var xmlAsStr = '<?xml version="1.0" encoding="utf-8" ?>'+x2js.json2xml_str( jsonObj );
+
+    console.log(xmlAsStr);
 
     var req = {
         method: 'POST',
-        url: 'http://127.0.0.1:8080/api/init/',
+        url: 'http://localhost:56019/api/analyse/codeDebt?repoUrl',
         headers: {
             'Content-Type': 'application/xml'
         },
         data: xmlAsStr
     };
-    $http(req).success(function (response) {
+    console.log("new");
+    console.log(req);
+    $http(req).then(function succ(response) {
         alert(response);
-    })
+        console.log(response);
+    }, function err(response) {
+        console.log(response);
+    });
+    console.log("function ended");
   }
 }]);
